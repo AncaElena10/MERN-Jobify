c5521021aff1134cd890881af726aa19672a4b11
Moisa Anca <moisa.anca10@gmail.com>
2023-05-16 14:27:49 +0300

    [MERN-Jobify] protected routes on backend

---
+++ messages/NOT_FOUND.js
@@ -0,0 +1,10 @@
+const PublicConstants = {
+    'E4040001': {
+        'code': 'E4040001',
+        'message': 'Route not found.',
+        'summary': 'The resource you are trying to access does not exist.',
+        'action': 'Please retry the operation with an existent route.'
+    },
+};
+
+module.exports = { ...PublicConstants };
--- middleware/app.middleware.js
+++ middleware/app.middleware.js
@@ -1,14 +1,39 @@
+const StatusCodes = require('http-status-codes').StatusCodes;
+const jwt = require('jsonwebtoken');
+
+const ErrorMessages = require('../messages');
+
 const PublicMethods = {
     notFound: (req, res) => {
-        console.log(`Got the req PATH ${req.originalUrl} that does not exist.`);
+        console.log(`[MIDDLEWARE] Got the req PATH ${req.originalUrl} that does not exist.`);
 
-        res.status(404).send('Route does not exist');
+        res.status(StatusCodes.NOT_FOUND).send(ErrorMessages.NOT_FOUND_MESSAGES.E4040001);
     },
     serverError: (error, req, res, next) => {
-        console.log(`Error occured while processing the request: ${error}\n${error.stack}`);
+        console.log(`[MIDDLEWARE] Error occured while processing the request: ${error}\n${error.stack}`);
 
-        res.status(500).send('An error occurred while processing the request.');
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(ErrorMessages.INTERNAL_SERVER_ERROR_MESSAGES.E5000001);
     },
+    auth: (req, res, next) => {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer')) {
+            console.log(`[MIDDLEWARE] User is not authenticated, unable to access the PATH ${req.originalUrl}.`);
+            return res.status(StatusCodes.UNAUTHORIZED).send(ErrorMessages.UNAUTHORIZED_MESSAGES.E4010002);
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        try {
+            const payload = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = { userId: payload.userId };
+
+            next();
+        } catch (error) {
+            console.error(`An error occurred while trying verify the token ${error}\n${error.stack}`);
+            return res.status(StatusCodes.UNAUTHORIZED).send(ErrorMessages.UNAUTHORIZED_MESSAGES.E4010002);
+        }
+    }
 }
 
 module.exports = { ...PublicMethods };
