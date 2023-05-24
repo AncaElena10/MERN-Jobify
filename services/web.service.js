5d3b8f49899cbd5b48ddec73c4ef8a99e5e08f71
Moisa Anca <moisa.anca10@gmail.com>
2023-05-24 21:33:42 +0300

    [MERN-Jobify] added some security packages, build client and limit register&login reqs

--- services/web.service.js
+++ services/web.service.js
@@ -3,6 +3,16 @@ const express = require('express');
 // const cors = require('cors');
 const morgan = require('morgan');
 
+// build app
+const path = require('path');
+const clientBuildPath = path.join(__dirname, '../client/build');
+const indexPath = path.join(clientBuildPath, 'index.html');
+
+// security packages
+const helmet = require('helmet');
+const xss = reqiure('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+
 const appRouter = require('../routes');
 const AppConstants = require('../config/constants');
 
@@ -23,8 +33,20 @@ const initialSetup = async () => {
     if (process.env.NODE_ENV !== 'production') {
         app.use(morgan('dev'));
     }
+
+    app.use(express.static(path.resolve(clientBuildPath)));
+
     app.use(express.json());
+
+    app.use(helmet); // secured headers
+    app.use(xss); // make sure the input is sanitized
+    app.use(mongoSanitize); // prevents mongodb operator injection
+
     app.use(appRouter, appMiddleware.notFound);
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(indexPath));
+    });
 };
 
 module.exports = {
